f720da2bc7265a0b25c412aa1347cd1a22148ab7
Alejandro <Work@alejandros-macbook-pro.local>
2016-09-01 19:07:54 -0700

    Cleaner Version of code.

--- Matchismo/ViewController.m
+++ Matchismo/ViewController.m
@@ -9,12 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-//@property (strong, nonatomic) CardMatchingGame *game;
-//@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @property (weak, nonatomic) IBOutlet UIButton *redealButton;
-//@property (weak, nonatomic) IBOutlet UISegmentedControl *gameModeSegmentControl;
-//@property (weak, nonatomic) IBOutlet UILabel *gameStatusLabel;
 
 @end
 
@@ -41,43 +37,29 @@
     int chosenButtonIndex = (int)[self.cardButtons indexOfObject:sender];
     [self.game chooseCardAtIndex:chosenButtonIndex];
     [self updateUI];
-    //[self enableGameMode:NO];
 }
 
 - (IBAction)touchRedealButton:(id)sender {
-    //self.scoreLabel.text = @"Re-dealing cards!";
     [self.game redealCards:12 withDeck:[self createDeck]];
-    //[self enableGameMode:YES];
     [self updateUI];
 }
 
-//- (IBAction)touchGameModeSement:(id)sender {
-//    BOOL mode = self.gameModeSegmentControl.selectedSegmentIndex;
-//    [self.game setGameMode: !mode];
-//    [self updateUI];
-//}
 
 - (void)updateUI{
     for(UIButton *cardButton in self.cardButtons){
         @try{
             int cardButtonIndex = (int)[self.cardButtons indexOfObject:cardButton];
             Card *card = [self.game cardAtIndex:cardButtonIndex];
-            //[cardButton setBackgroundImage:[self backgroundImageForCard:card] forState:UIControlStateNormal];
             cardButton.enabled = !card.isMatched;
             self.scoreLabel.text = [NSString stringWithFormat:@"Score: %d", (int)self.game.score];
         }@catch (NSException * e) {
             NSLog(@"Exception: %@", e);
         }
     }
-//    self.gameStatusLabel.text = @"";
-//    self.gameStatusLabel.text = self.game.gameStatus;
 }
 
 
-//-(void) enableGameMode: (BOOL) enable{
-//    [_gameModeSegmentControl setEnabled:enable forSegmentAtIndex:0];
-//    [_gameModeSegmentControl setEnabled:enable forSegmentAtIndex:1];
-//}
+
 
 
 
